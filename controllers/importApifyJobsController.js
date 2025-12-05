9d7ad3e31b4f818a83eb127e938249a10bd9320e
PirateKingAkshit <akshitkumar383@gmail.com>
2025-12-05 18:46:35 +0530

    apify-job-sync

--- controllers/importApifyJobsController.js
+++ controllers/importApifyJobsController.js
@@ -1,33 +1,44 @@
-// controllers/apifyImport.controller.js
-
 const importApifyJobs = require("../services/importApifyJobsService");
 const { fetchApifyJobs } = require("../utils/apifyClient");
-const jobData = require('./data');
-/**
- * Controller that:
- * 1. Fetches jobs from Apify
- * 2. Transforms jobs
- * 3. Saves into DB
- */
+const dotenv = require("dotenv").config();
+const CryptoJS = require("crypto-js");
+
+function decrypt(encryptedText) {
+  const bytes = CryptoJS.AES.decrypt(encryptedText, process.env.ENCRYPTION_SECRET);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
+
 exports.importFromApify = async (req, res) => {
   try {
-    // const { apifyUrl } = req.body;
     const employerId = req.user.id;
+    const { encryptedDatasetId } = req.body;
 
-    // if (!apifyUrl) {
-    //   return res.status(400).json({ error: "apifyUrl is required" });
-    // }
+    if (!encryptedDatasetId) {
+      return res.status(400).json({ error: "encryptedDatasetId is required" });
+    }
 
-    // 1. Fetch jobs from Apify API
-    //const apifyJobsArray = await fetchApifyJobs(apifyUrl);
-    const apifyJobsArray = jobData.jobs;
-    // 2. Transform + Save
+    // 1. Decrypt datasetId
+    const datasetId = decrypt(encryptedDatasetId);
+
+    if (!datasetId) {
+      return res.status(400).json({ error: "Invalid encrypted datasetId" });
+    }
+
+    // 2. Build Apify dataset URL
+    const apifyUrl = `https://api.apify.com/v2/datasets/${datasetId}/items?clean=1&format=json&token=${process.env.APIFY_TOKEN}`;
+
+    // 3. Fetch jobs from Apify
+    const apifyJobsArray = await fetchApifyJobs(apifyUrl);
+
+    // 4. Import into DB
     const summary = await importApifyJobs(apifyJobsArray, employerId);
 
     res.status(200).json({
       message: "Import completed",
       summary,
+      itemsReceived: apifyJobsArray.length,
     });
+
   } catch (err) {
     res.status(500).json({
       error: "Apify import failed",
